bbb226272cdd492452336087d2e79157d5664228
Andrés Forns Jusino <andresito.jusino@gmail.com>
2022-04-11 19:43:28 -0400

    First commit!

---
+++ src/main/java/com/tko/btcnotifier/BtcnotifierApplication.java
@@ -0,0 +1,13 @@
+package com.tko.btcnotifier;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BtcnotifierApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BtcnotifierApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tko/btcnotifier/BtcnotifierApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tko.btcnotifier;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BtcnotifierApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
