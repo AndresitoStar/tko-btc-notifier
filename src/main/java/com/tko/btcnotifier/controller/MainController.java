887c5499117d2387a1a259ee81e68ad39dd82301
Andrés Forns Jusino <andresito.jusino@gmail.com>
2022-04-11 20:50:43 -0400

    First commit!

--- src/main/java/com/tko/btcnotifier/controller/MainController.java
+++ src/main/java/com/tko/btcnotifier/controller/MainController.java
@@ -5,11 +5,18 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/btcNotifier")
 public class MainController {
+    @RequestMapping("/")
+    @ResponseBody
+    String home() {
+        return "Hello World, this is BTC Status Notifier - Made by Tko!";
+    }
+
     @GetMapping
     public ResponseEntity<String> helloWorld() {
         return new ResponseEntity<String>("Hello World", new HttpHeaders(), HttpStatus.OK);
