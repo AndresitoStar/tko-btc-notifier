4186e2e76b4cfa17315dc611e46cfc589085b02a
Andrés Forns Jusino <andresito.jusino@gmail.com>
2022-04-11 20:17:31 -0400

    First commit!

---
+++ src/main/java/com/tko/btcnotifier/controller/MainController.java
@@ -0,0 +1,17 @@
+package com.tko.btcnotifier.controller;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/btcNotifier")
+public class MainController {
+    @GetMapping
+    public ResponseEntity<String> helloWorld() {
+        return new ResponseEntity<String>("Hello World", new HttpHeaders(), HttpStatus.OK);
+    }
+}
